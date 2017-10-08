2dafa36f55639df75947cc335b42b1c9813be8a2
Joseph Dalmasso <me@jodalmasso.com>
2017-10-08 17:13:11 -0400

    testing

--- src/page/Header.js
+++ src/page/Header.js
@@ -5,7 +5,7 @@ import FontAwesome from 'react-fontawesome';
 
 const Header = () => {
     return (
-        <header className="flex direction-row justify-between text-align">
+        <header className="flex justify-between text-align">
         	<Link to="/"><div className="menu"><FontAwesome name="home" size="2x"/></div></Link>
         	<Link to="/game"><div className="menu"><FontAwesome name="rocket" size="2x"/></div></Link>
         </header>
