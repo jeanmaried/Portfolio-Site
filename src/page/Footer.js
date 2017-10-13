59b2f5930574f9f65199523c03eb30c0057241e8
Joseph Dalmasso <me@jodalmasso.com>
2017-10-13 14:35:56 -0400

    switched up header/footer

--- src/page/Footer.js
+++ src/page/Footer.js
@@ -6,8 +6,8 @@ import FontAwesome from 'react-fontawesome';
 const Footer = () => {
     return (
     	<footer className="flex direction-row justify-between text-align">
-	    		<Link to="/projects"><div className="menu"><FontAwesome name="briefcase" size="2x"/></div></Link>
-				<Link to="/about"><div className="menu"><FontAwesome name="question" size="2x"/></div></Link>
+			<Link to="/about"><div className="menu"><FontAwesome name="question" size="2x"/></div></Link>
+			<Link to="/contact"><div className="menu"><FontAwesome name="phone" size="2x"/></div></Link>
     	</footer>
     );
 }
--- src/page/Header.js
+++ src/page/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <header className="flex justify-between text-align">
         	<Link to="/"><div className="menu"><FontAwesome name="home" size="2x"/></div></Link>
-        	<Link to="/contact"><div className="menu"><FontAwesome name="phone" size="2x"/></div></Link>
+            <Link to="/projects"><div className="menu"><FontAwesome name="briefcase" size="2x"/></div></Link>
         </header>
     );
 }
