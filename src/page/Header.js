b0b415e643a4999cb93be6d1c313950c0d72a4c9
Joseph Dalmasso <me@jodalmasso.com>
2017-10-24 14:05:59 -0400

    added meta

--- src/page/Header.js
+++ src/page/Header.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import '../styles/flex.css';
 import FontAwesome from 'react-fontawesome';
-import Meta from './Meta';
 
 const Header = () => {
     return (
         <header className="flex justify-between text-align">
-            <Meta />
         	<Link to="/"><div className="menu"><FontAwesome name="home" size="2x"/></div></Link>
             <Link to="/projects"><div className="menu"><FontAwesome name="briefcase" size="2x"/></div></Link>
         </header>
