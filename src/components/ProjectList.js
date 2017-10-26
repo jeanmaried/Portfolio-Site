ab3da69c7b91adcc8603023e50f5fa8abc5187a7
Joseph Dalmasso <me@jodalmasso.com>
2017-10-26 14:19:04 -0400

    Project page looks better now

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -9,13 +9,16 @@ import '../styles/projects.css'
 const ProjectList = function({item}) {
   let background = {backgroundImage: `url(${item.picture})`};
   return (
-  // let background = {backgroundImage: `url(../assets/${item.picture})`};
-  <li className="project text-align" id={item.id}>
+  <li className="project text-align flex align-items-center direction-column" id={item.id}>
       <h1 className="yellow">{item.title}</h1>
-      <div className="background" style={background}></div>
-      <a target="_blank" href={item.site_link}><FontAwesome name="mouse-pointer" size="2x"/></a>
-      <a target="_blank" href={item.git_link}><FontAwesome name="github" size="2x"/></a>
-      <p>{item.description}</p>
+      <div className="flex align-items-center direction-column">
+        <div className="background" style={background}></div>
+        <p>{item.description}</p>
+        <div className="project_links flex justify-around">
+          <a target="_blank" href={item.site_link}><FontAwesome name="mouse-pointer" size="2x"/></a>
+          <a target="_blank" href={item.git_link}><FontAwesome name="github" size="2x"/></a>
+        </div>
+  </div>
   </li>
 )}
 
