b29770b52b1cd6b1bd27c552f21b188b833dba9c
Joseph Dalmasso <me@jodalmasso.com>
2017-11-02 20:36:27 -0400

    changed name

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,7 +11,7 @@ class About extends Component {
               <div className="about_me flex direction-column align-items-end">
               <div className="title_view_container">
                 <div className="about_title text-end title_space yellow">
-                  <h1>Joseph Dalmasso</h1>
+                  <h1>Jean Dalmasso</h1>
                   <p><em>Full-Stack Developer</em></p>
                   <p><em>Toronto, ON</em></p>
                 </div>
@@ -20,7 +20,7 @@ class About extends Component {
                     <p>After 3 years in sales I quit my job, moved to a new country, and did a lot of 
                     soul searching before realizing my true calling was Web Development. Finally a career that allows me to apply my creativity and logic!</p>
                     <p>When I'm not coding, you can find me dancing like a fool, listening to music or 
-                    meditating.</p>
+                    meditating. Know french? On est déjà amis!</p>
                     <p>Feel free to click around and see some of my work!</p>
                   </div>
 
