a424f989aac04e8ec7a3b9156fe1ab89da48b017
Joseph Dalmasso <me@jodalmasso.com>
2017-11-09 20:47:59 -0500

    Changed about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -18,7 +18,7 @@ class About extends Component {
                     soul searching before realizing my true calling was Web Development. Finally a career that allows me to apply my creativity and logic!</p>
                     <p>When I'm not coding, you can find me dancing like a fool, listening to music or 
                     meditating. Know french? On est déjà amis!</p>
-                    <p>Feel free to click around and see some of my work!</p>
+                    <p>Feel free to click the icons in the corners to learn more about me and see my work!</p>
                   </div>
 
                   <div className="skills flex direction-row justify-center flex-wrap">
