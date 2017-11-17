60c0e5952a221c7d65bc1181b170269f4743fc8e
Joseph Dalmasso <me@jodalmasso.com>
2017-11-16 19:28:12 -0500

    testing focus

--- src/components/chatbot/ChatBot.js
+++ src/components/chatbot/ChatBot.js
@@ -7,10 +7,10 @@ import ChatBot from 'react-simple-chatbot';
 const theme = {
   background: '#f5f8fb',
   fontFamily: 'Helvetica Neue',
-  headerBgColor: '#EF6C00',
+  headerBgColor: 'grey',
   headerFontColor: '#fff',
   headerFontSize: '15px',
-  botBubbleColor: '#EF6C00',
+  botBubbleColor: 'grey',
   botFontColor: '#fff',
   userBubbleColor: '#fff',
   userFontColor: '#4a4a4a',
