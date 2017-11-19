1e2beadf3ef5e6959805c1124c6d644be57578f1
Joseph Dalmasso <me@jodalmasso.com>
2017-11-19 12:07:36 -0500

    TEst

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,32 +8,27 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-        {/* <div className="earth_box">
-          <div className="earth text-align"></div>
-          <div className="animate2">
-              <div className="moon"><img src={require("../../assets/moon2.png")} alt=""/></div>
-          </div>
-        </div> */}
 
       <div className="about_box flex justify-end">
       <div className="about_view_container flex direction-column align-items-center">
 
-      <div className="earth text-align"></div>
-          <div className="animate2">
-              <div className="moon"><img src={require("../../assets/moon2.png")} alt=""/></div>
-          </div>
+      <h2 className="yellow about_title">Who am I?</h2>
 
-        <h1 className="text-align yellow">Who am I?</h1>
+        <div className="earth text-align"></div>
+        <div className="animate2">
+            <div className="moon"><img src={require("../../assets/moon2.png")} alt=""/></div>
+        </div>
 
         <div>
-          <p>My name is Joseph but everyone calls me by my middle name Jean. I am a Web Developer based in Toronto, ON.</p>
-          <p>After 3 years in sales I quit my job, moved to a new country, and did a lot of 
-          soul searching before realizing my true calling was Web Development. Finally a career that allows me to apply my creativity and logic!</p>
+          <p>I am a Freelance Web Developer based in Toronto, ON.</p>
+          <p>With a background in sales and customer service, I switched over to web development to allow myself to apply my creativity and logic in a more meaningfull way.
+            I am currently learning Node.js and have a mad passion for coding in React.js.</p>
           <p>When I'm not coding, you can find me dancing like a fool, listening to music or 
           meditating. Know french? On est déjà amis!</p>
-          <div className="flex justify-center">
+          I'm currently available for freelance and contract work opportunities.
+          {/* <div className="flex justify-center">
               <Link to="/projects"><div className="red_button text-align">See my work!</div></Link>
-          </div>
+          </div> */}
         </div>
       </div>
       </div>
