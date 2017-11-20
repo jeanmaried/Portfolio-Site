d734df9781659c8bd9f71320da951fca689eb770
Joseph Dalmasso <me@jodalmasso.com>
2017-11-19 23:15:55 -0500

    skills page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,30 +8,28 @@ class Skills extends Component {
         return (
         <div className="skills white flex direction-column align-items-center flex-wrap">
             <h2 className="skills_title text-align">Skills</h2>
-            <div className="flex direction-row text-align">
-                <div><i className="devicon-html5-plain"></i></div>
-                <div><i className="devicon-css3-plain"></i></div>
-                <div><i className="devicon-sass-plain"></i></div>
-                <div><i className="devicon-javascript-plain"></i></div>
-                <div><i className="devicon-jquery-plain"></i></div>
-            </div>   
-            <div className="flex direction-row text-align"> 
-                <div><i className="devicon-wordpress-plain"></i></div>
-                <div><i className="devicon-php-plain"></i></div>
-                <div><i className="devicon-github-plain"></i></div>
-                <div><i className="devicon-gulp-plain"></i></div>
-            </div>   
-            <div className="flex direction-row text-align">
-                <div><i className="devicon-react-plain"></i></div>
-                <div><i className="devicon-mongodb-plain"></i></div>
-                <div><i className="devicon-nodejs-plain"></i></div>
+            <div className="flex flex-wrap text-align">
+                <i className="devicon-html5-plain"></i>
+                <i className="devicon-css3-plain"></i>
+                <i className="devicon-sass-plain"></i>
+                <i className="devicon-javascript-plain"></i>
+                <i className="devicon-jquery-plain"></i>
+                <i className="devicon-wordpress-plain"></i>
+                <i className="devicon-php-plain"></i>
+                <i className="devicon-github-plain"></i>
+                <i className="devicon-gulp-plain"></i>
+                <i className="devicon-react-plain"></i>
+                <i className="devicon-mongodb-plain"></i>
             </div>
-                <div className="flex direction-row text-align">
-                <div><i className="devicon-express-original"></i></div>
-                <div><i className="devicon-postgresql-plain"></i></div>
-            </div>
+                {/* <div><i className="devicon-postgresql-plain"></i></div> */}
+
             <div>
                 <div><i className="devicon-meteor-plain"></i></div>
+            </div>
+            <h3 className="skills_title text-align">Learning</h3>
+            <div className="flex flex-wrap text-align">
+                <i className="devicon-nodejs-plain"></i>
+                <i className="devicon-express-original"></i>
             </div>
         </div>
         );
