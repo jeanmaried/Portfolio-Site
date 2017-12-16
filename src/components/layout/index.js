73238acd73811c1556c7293e014f8271c4447605
Joseph Dalmasso <me@jodalmasso.com>
2017-12-16 11:53:13 -0500

    test

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,15 +3,27 @@ import Footer from './Footer';
 import Header from './Header';
 import Loader from '../Loader';
 import { connect } from 'react-redux';
+import Particles from '../Particles';
 
 import { withRouter } from 'react-router-dom';
 
 class Layout extends Component {
   render() {
-    console.log('Hello', this.props);
+    let particles;
+    if (
+      (navigator.userAgent.indexOf('Opera') ||
+        navigator.userAgent.indexOf('OPR')) != -1 ||
+      navigator.userAgent.indexOf('Chrome') != -1 ||
+      navigator.userAgent.indexOf('Firefox') != -1
+    ) {
+      console.log('Not IE :)');
+    } else {
+      particles = <Particles />;
+    }
     return (
       <div>
         {this.props.isLoading ? <Loader /> : null}
+        {particles}
         {this.props.location.pathname == '/' ? null : <Header />}
         {this.props.children}
         {this.props.location.pathname == '/' ? null : <Footer />}
